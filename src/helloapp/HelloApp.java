a251a251a44d5c630b549dbaad0fabed6d1385d7
panospagiatis <panos.payiatis@gmail.com>
2025-05-14 20:28:56 +0300

    feat: greet user based on time of day

--- src/helloapp/HelloApp.java
+++ src/helloapp/HelloApp.java
@@ -1,6 +1,7 @@
 package helloapp;
 
 import java.util.Scanner;
+import java.time.LocalTime;
 
 /**
  * A simple program that prints to standard output
@@ -9,12 +10,22 @@ import java.util.Scanner;
  */
 public class HelloApp {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is your name? ");
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + name + "!");
+        LocalTime now = LocalTime.now();        // gets the current local time
+        int hour = now.getHour();               // gets the hour (0 to 23)
+
+        if (hour < 12) {
+            System.out.println("Good morning, " + name + "!");
+        } else if (hour < 18) {
+            System.out.println("Good afternoon, " + name + "!");
+        } else {
+            System.out.println("Good evening, " + name + "!");
+        }
     }
 }
 
